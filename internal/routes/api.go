20a3551b6d09b7823d3cba3889f6e222281bf6c9
fiyosa <fiyosa97@gmail.com>
2026-06-08 10:21:47 +0700

    feat: implement policy provider middleware and register authorized policy routes

--- internal/routes/api.go
+++ internal/routes/api.go
@@ -3,12 +3,14 @@ package routes
 import (
 	"go-fiber-svelte/internal/http/controllers"
 	"go-fiber-svelte/internal/middleware"
+	"go-fiber-svelte/internal/provider"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func RegisterAPI(app *fiber.App) {
 	auth := middleware.AuthMiddleware()
+	policy := provider.Policy
 	api := app.Group("/api")
 
 	api.Get("/openapi.json", controllers.OpenAPI)
@@ -21,8 +23,8 @@ func RegisterAPI(app *fiber.App) {
 	authGrp.Get("/user", auth, controllers.User)
 
 	policyGrp := api.Group("/policy", auth)
-	policyGrp.Get("/role", controllers.RoleList)
-	policyGrp.Get("/permission", controllers.PermissionList)
-	policyGrp.Post("/permission", controllers.PermissionStore)
-	policyGrp.Delete("/permission/:id", controllers.PermissionDestroy)
+	policyGrp.Get("/role", policy("role-list"), controllers.RoleList)
+	policyGrp.Get("/permission", policy("permission-list"), controllers.PermissionList)
+	policyGrp.Post("/permission", policy("permission-store"), controllers.PermissionStore)
+	policyGrp.Delete("/permission/:id", policy("permission-destroy"), controllers.PermissionDestroy)
 }
